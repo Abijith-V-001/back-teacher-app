be0c5241ec28a729556825e6e60fe0ad12fbcc29
Abijith-V-001 <abivandiyil001@gmail.com>
2024-06-12 21:36:57 +0530

    search teacher branch data added

--- App.js
+++ App.js
@@ -7,12 +7,24 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 
+mongoose.connect("mongodb+srv://abijith0a31:abijith1415@cluster0.fodkdeb.mongodb.net/teacherDB?retryWrites=true&w=majority&appName=Cluster0")
+
+
 app.post("/add",(req,res)=>{
     let input=req.body
     let Teacher=new teachermodel(input)
     Teacher.save()
     console.log(Teacher)
     res.json({"status":"success"})
+})
+
+app.post("/search",(req,res)=>{
+    let input=req.body
+    teachermodel.find(input).then(
+        (data)=>{
+            res.json(data)
+        }
+    )
 })
 
 app.listen(8080,()=>{
